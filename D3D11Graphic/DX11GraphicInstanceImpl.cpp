3c943be7bb9e91aa7a31f20635b4de1b14e09c7f
wang.shaohui <wang.shaohui@navercorp.com>
2022-11-01 13:18:17 +0800

    save

--- D3D11Graphic/DX11GraphicInstanceImpl.cpp
+++ D3D11Graphic/DX11GraphicInstanceImpl.cpp
@@ -430,14 +430,12 @@ bool DX11GraphicInstanceImpl::InitSamplerState()
 	CHECK_GRAPHIC_CONTEXT;
 
 	D3D11_SAMPLER_DESC samplerDesc = {};
-	samplerDesc.Filter = D3D11_FILTER_ANISOTROPIC;
-	samplerDesc.MaxAnisotropy = 16;
+	samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
 	samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
 	samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
 	samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
 	samplerDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
-	samplerDesc.MinLOD = 0;
-	samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
+	samplerDesc.MaxLOD = FLT_MAX;
 
 	HRESULT hr = m_pDX11Device->CreateSamplerState(&samplerDesc, m_pSampleState.Assign());
 	if (FAILED(hr)) {
