478f2ab9791f03574cd5ad85289510b735c4d5b9
wang.shaohui <wang.shaohui@navercorp.com>
2022-11-01 10:56:27 +0800

    save

--- MFCDemo/convert-video-define.h
+++ MFCDemo/convert-video-define.h
@@ -14,6 +14,6 @@ struct video_convert_params {
 	uint32_t width = 0;
 	uint32_t height = 0;
 	enum AVPixelFormat format = AVPixelFormat::AV_PIX_FMT_NONE;
-	enum video_range_type color_range = video_range_type::VIDEO_RANGE_FULL;
-	enum video_colorspace color_space = video_colorspace::VIDEO_CS_709;
+	enum video_range_type color_range = video_range_type::VIDEO_RANGE_PARTIAL;
+	enum video_colorspace color_space = video_colorspace::VIDEO_CS_601;
 };
